9c9e58a814d7c6ae652bcc3037bbdb09fcef89c0
Krisha11 mail = vfhbyf2@yandex.ru <vfhbyf2@yandex.ru>
2018-01-13 01:01:42 +0500

    Message

--- app/src/main/java/com/example/asus/taskmanager/TasksList.java
+++ app/src/main/java/com/example/asus/taskmanager/TasksList.java
@@ -29,6 +29,11 @@ public class TasksList {
         return arrayList.get(index);
     }
 
+    public int getSize()
+    {
+        return arrayList.size();
+    }
+
     public void delete(int index) {
         this.arrayList.remove(index);
         Collections.sort(arrayList, new Comparator<Task>() {
