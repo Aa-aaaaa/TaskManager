79e6c94f61d18b4517c7f8d882929805409703af
Cepryn9 <sergeypospelov59@gmail.com>
2018-01-12 20:37:10 +0300

    Added TaskList

--- app/src/main/java/com/example/asus/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/asus/taskmanager/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.asus.taskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String[] strings = {"Task1", "Task2", "Task3", "Task4"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strings);
+        ((ListView)findViewById(R.id.taskList)).setAdapter(adapter);
     }
 }
