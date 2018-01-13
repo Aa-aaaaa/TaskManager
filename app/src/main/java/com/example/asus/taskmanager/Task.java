f6469471edc71b41f666572de14e8a2a6d9b5c59
Krisha11 mail = vfhbyf2@yandex.ru <vfhbyf2@yandex.ru>
2018-01-13 18:02:33 +0500

    Time

--- app/src/main/java/com/example/asus/taskmanager/Task.java
+++ app/src/main/java/com/example/asus/taskmanager/Task.java
@@ -1,5 +1,6 @@
 package com.example.asus.taskmanager;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Task {
@@ -17,6 +18,30 @@ public class Task {
         this.time = time;
         this.description = description;
         this.name = name;
+    }
+
+    public boolean stringToTime(String s) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        Date t = new Date(), timeNow, timeFromString;
+        String stringTimeNow = simpleDateFormat.format(time), s2 = "";
+        try {
+            timeNow = simpleDateFormat.parse(stringTimeNow);
+            timeFromString = simpleDateFormat.parse(s);
+            if (timeNow.compareTo(timeFromString) >= 0
+                    || (simpleDateFormat.format(timeFromString) != s))
+                return false;
+            time.setTime(t.getTime() + timeFromString.getTime() - timeNow.getTime());
+        }
+        catch (Exception e) {
+            return false;
+        };
+        return true;
+    }
+
+    public String timeToString()
+    {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        return simpleDateFormat.format(time);
     }
 
     public String getDescription() {
