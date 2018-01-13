044dd2377871e94065b444e0f6dee12860cc85a2
Cepryn9 <sergeypospelov59@gmail.com>
2018-01-13 16:08:17 +0300

    Connected MainActtivity with TaskShow

--- app/src/main/java/com/example/asus/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/asus/taskmanager/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -18,19 +19,31 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         taskList.insert(new Task("MyTask1", new Date(1337 * 1000 * 60 * 60), "Easy"));
         taskList.insert(new Task("MyTask2", new Date(1488 * 1000 * 60 * 60), "Hard"));
         taskList.insert(new Task("MyTask3", new Date(322 * 1000 * 60 * 60), "Very-Hard"));
         taskListAdapter = new TaskListAdapter(taskList);
 
+        ((ListView)findViewById(R.id.taskList)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, TaskShowActivity.class);
+                intent.putExtra("name", taskList.getTask(i).getName());
+                intent.putExtra("time", taskList.getTask(i).getTime().toString());
+                intent.putExtra("description", taskList.getTask(i).getDescription());
+                startActivity(intent);
+            }
+        });
         ((ListView)findViewById(R.id.taskList)).setAdapter(taskListAdapter);
+
         ((Button)findViewById(R.id.buttonGoMakeNewTask)).
                 setOnClickListener(new View.OnClickListener()
                 {
                     @Override
                     public void onClick(View view)
                     {
-                        Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
+                        Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
                         startActivity(intent);
                     }
                 }
--- app/src/main/java/com/example/asus/taskmanager/TaskShowActivity.java
+++ app/src/main/java/com/example/asus/taskmanager/TaskShowActivity.java
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class TaskShow extends AppCompatActivity {
+public class TaskShowActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_show);
+        setContentView(R.layout.activity_show_task);
         TextView name = findViewById(R.id.name);
         TextView description = findViewById(R.id.description);
         TextView time = findViewById(R.id.time);
